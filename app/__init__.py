bf720b1155efd08765b8210a99a11afafa4a4153
Sankalp <sankalp.313@gmail.com>
2023-03-16 17:18:30 -0700

    Added logic to marking Texts to Translate In Templates

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ import os
 from flask_mail import Mail
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
-from flask_babel import Babel
+from flask_babel import Babel, lazy_gettext as _l
 from flask import request
 
 app = Flask(__name__)
@@ -19,6 +19,7 @@ db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
 login.login_view = 'login'
+login.login_message = _l('Please log in to access this page.')
 mail = Mail(app)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
@@ -56,5 +57,5 @@ if not app.debug:
 def get_locale():
     # return request.accept_languages.best_match(app.config['LANGUAGES'])
     return 'es'
-    
+
 from app import routes, models, errors
